fe997bfd4241882d6bfe3796242df14759793a03
Shashi Raja <sraja@Ban-1Sraja-m.local>
2018-08-25 21:30:52 +0530

    inlcuded src and test

---
+++ src/naughty-string-validator.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const naughtyStringList = require('../src/naughty-string-collection/blns.json');
+
+exports.getRandomNaughtyString = () => {
+    const stringLength = naughtyStringList.length;
+    const randomIndex = Math.floor(Math.random() * (stringLength - 0) + 0);
+    return naughtyStringList[randomIndex];
+};
+
+exports.getNaughtyStringList = () => {
+    return naughtyStringList;
+}; 
---
+++ test/test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const fs = require('fs');
+const { getRandomNaughtyString, getNaughtyStringList } = require('../src/naughty-string-validator');
+
+describe("#naughty-string-collection", function () {
+    it("should check blns.json exists", function () {
+        expect(fs.existsSync('src/naughty-string-collection/blns.json')).to.be.true;
+    });
+});
+
+describe('#naughty-string-validator', function () {
+    it('should return random naughty string', function () {
+        var randomString = getRandomNaughtyString();
+        expect(randomString).to.be.a('string');
+    });
+    it('should return naughty string list', function () {
+        var naughtyStringList = getNaughtyStringList();
+        expect(naughtyStringList).to.be.an('array').that.is.not.empty;
+    });
+});
